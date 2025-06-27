19c81a5d57c7fd41bf17d3a7a1b6748ca92c165e
Simon <simongomezarango123@gmail.com>
2025-06-26 20:35:57 -0500

    feat: enhance LanguageSwitcher to support dynamic route parameters and add ProjectPage component

--- src/Components/LanguageSwitcher/LanguageSwitcher.tsx
+++ src/Components/LanguageSwitcher/LanguageSwitcher.tsx
@@ -1,35 +1,30 @@
 "use client";
 
 import { Link, useRoute } from "next-globe-gen";
+import { useParams } from "next/navigation";
 import { SpainFlag, UKFlag } from "../SVG/Flags";
 
-/**
- * If there is dynamic route segments in some of the application routes (i.e. "/images/[id]"),
- * the params provided by Next.js useParams function have to be passed as a prop to
- * Link components for language switching to work properly.
- */
 export default function LanguageSwitcher() {
   const route = useRoute();
+  const params = useParams();
 
+  const id = params?.id;
+  const dynamicParams = typeof id === "string" ? { id } : undefined;
+  
   return (
     <nav>
       <ul className="flex flex-row gap-4 w-full items-center">
-        
         <li>
-          <Link href={route} locale="en" className="flex items-center gap-2">
+          <Link href={route} locale="en" params={dynamicParams} className="flex items-center gap-2">
             <UKFlag className="w-5 h-auto flex-shrink-0 align-middle" />
           </Link>
         </li>
-
         <li>
-          <Link href={route} locale="es" className="flex items-center gap-2">
+          <Link href={route} locale="es" params={dynamicParams} className="flex items-center gap-2">
             <SpainFlag className="w-5 h-auto flex-shrink-0 align-middle" />
           </Link>
         </li>
-
       </ul>
     </nav>
   );
 }
-
-
